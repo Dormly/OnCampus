636846af45ceb92c828af20dd2463ab1f7116820
BuildTools <unconfigured@null.spigotmc.org>
2025-01-12 20:45:05 -0600

    This is my first commit

---
+++ app/housing.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export default function Page() {
+  return <Text>Housing Room & Board</Text>;
+}
--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,6 +13,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'xmark.triangle.circle.square.fill': 'dashboard',
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
